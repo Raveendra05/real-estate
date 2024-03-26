0006606e19be9d7e54858514bd394a2aec204b7c
Raveendra05 <ravindrapatidar272@gmail.com>
2024-03-26 18:48:05 +0530

    add profile page

--- client/src/component/header.jsx
+++ client/src/component/header.jsx
@@ -29,7 +29,7 @@ export default function Header() {
                 <Link to='/profile'>
                 {
                     currentUser ? (
-                        <img  className='rounded-lg h-8 w-8 object-cover' src={currentUser.avatar} alt="no-image" />
+                        <img  className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="no-image" />
                     ):
                     (
                         <li className=' text-slate-700 hover:underline'>Sign In</li>
