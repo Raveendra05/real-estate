c0f6cf2f7e7ab8a96a401053cd304eafb814ea7a
Raveendra05 <ravindrapatidar272@gmail.com>
2024-04-04 17:17:29 +0530

    about page

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About Patidar Estate
+      </h1>
+      <p className='mb-4 text-slate-700'>Welcome to our estate website, your premier destination for all things related to real estate!
+        Whether you're in search of your dream home, looking to sell your property, or seeking expert 
+        advice on investment opportunities, we've got you covered. Our platform offers a comprehensive 
+        range of services tailored to meet your individual needs, backed by a team of seasoned professionals 
+        with a wealth of experience in the industry.</p>
+        <br />
+        <p className='mb-4 text-slate-700'>
+        At our estate website, we understand that buying or selling a property is one of the most significant
+         decisions you'll ever make. That's why we provide a user-friendly interface coupled with powerful 
+         search tools to simplify your property search or listing process. From luxurious villas to cozy apartments,
+          our extensive database features a diverse selection of properties to suit every taste and budget. Whether 
+          you're a first-time buyer or a seasoned investor, our website serves as your one-stop destination for all 
+          things real estate, ensuring a seamless and rewarding experience from start to finish.
+        </p>
+    </div>
   )
 }
